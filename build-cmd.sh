36caa8688623c65f4d248a39822e395537ac437d
Vector <vector@alchemyviewer.org>
2025-02-26 05:20:06 -0500

    Attempt to use SCM versioning

--- build-cmd.sh
+++ build-cmd.sh
@@ -35,6 +35,5 @@ pushd "$TINYGLTF_SOURCE_DIR"
     cp -a json.hpp "$stage/include/tinygltf"
     mkdir -p "$stage/LICENSES"
     cp -a LICENSE "$stage/LICENSES/tinygltf_license.txt"
-    echo "v2.8.21" > "$stage/include/tinygltf/tinygltf_version.txt"
 popd
 
